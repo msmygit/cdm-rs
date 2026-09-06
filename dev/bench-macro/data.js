5947510e0e925c0dbcc2badf2d6dd1d277f8d24f
github-action-benchmark <github@users.noreply.github.com>
2026-09-06 09:16:06 +0000

    add Benchmark (cargo) benchmark result for 72b38003bb33518bd7e89c34457eb88b6b5b93e0

--- dev/bench-macro/data.js
+++ dev/bench-macro/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788085493834,
+  "lastUpdate": 1788686166935,
   "repoUrl": "https://github.com/msmygit/cdm-rs",
   "entries": {
     "Benchmark": [
@@ -82,6 +82,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "nfr_004_macro_migrate",
             "value": 167346,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Madhavan",
+            "username": "msmygit",
+            "email": "msmygit@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "72b38003bb33518bd7e89c34457eb88b6b5b93e0",
+          "message": "Merge pull request #92 from msmygit/dependabot/github_actions/taiki-e/install-action-2.85.13\n\nchore(deps): bump taiki-e/install-action from 2.85.9 to 2.85.13",
+          "timestamp": "2026-08-19T01:27:21Z",
+          "url": "https://github.com/msmygit/cdm-rs/commit/72b38003bb33518bd7e89c34457eb88b6b5b93e0"
+        },
+        "date": 1788686165933,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "nfr_004_macro_migrate",
+            "value": 108607,
             "range": "± 0",
             "unit": "ns/iter"
           }
